f1cd7195b279dbf131c5bb28be9755be4c59f3e6
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-11 14:25:18 -0800

    feat: Add GameCard composable
    Adds a new `GameCard` composable to display individual game information.
    
    Key features:
    - Shows the game's background image using `SubcomposeAsyncImage` with placeholder and error states.
    - Displays the game's name, rating with a star icon, and release year over a gradient overlay at the bottom.
    - The entire card is clickable.

---
+++ app/src/main/java/com/example/gamervault/features/games/components/GameCard.kt
@@ -0,0 +1,108 @@
+package com.example.gamervault.features.games.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import com.example.gamervault.R
+import com.example.gamervault.domain.models.Game
+import com.example.gamervault.features.search.components.BodyLarge
+import com.example.gamervault.features.search.components.BodyMedium
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun GameCard(
+    game: Game,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    val colorWhite =  Color(0xFFe0e7ff)
+    Box( modifier = modifier.clickable { onClick() }.clip(shape = RoundedCornerShape(8.dp)) ) {
+        SubcomposeAsyncImage(
+            model = game.backgroundImage,
+            contentDescription = "${game.name}_image_subCompose",
+            modifier = Modifier.aspectRatio(1f),
+            contentScale = ContentScale.Crop,
+            loading = {
+                Image(
+                    painter = painterResource(R.drawable.icon_image),
+                    contentDescription = "image_${game.name}",
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimary),
+                    modifier = Modifier.aspectRatio(1f).testTag("image_${game.name}"),
+                    contentScale = ContentScale.Crop
+                )
+            },
+            error = {
+                Image(
+                    painter = painterResource(R.drawable.icon_no_image),
+                    contentDescription = "no_image_${game.name}",
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimary),
+                    modifier = Modifier.aspectRatio(1f).testTag("no_image_${game.name}"),
+                    contentScale = ContentScale.Crop
+                )
+            },
+            success = {
+                SubcomposeAsyncImageContent()
+            }
+
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color.Transparent, Color.Black.copy(alpha = 0.5f), Color.Black.copy(alpha = 0.7f), Color.Black)
+                    )
+                )
+                .padding(4.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+
+            ) {
+            BodyLarge(text = game.name, modifier = Modifier.weight(1f), color = colorWhite , maxLines = 2)
+            Spacer(modifier = Modifier.width(2.dp))
+            Column( horizontalAlignment = Alignment.End) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(2.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.icon_star),
+                        contentDescription ="${game.rating}_stars",
+                        tint = MaterialTheme.colorScheme.onTertiary,
+                        modifier = Modifier.size(16.dp)
+                    )
+                    BodyMedium(game.rating.toString(),color = colorWhite )
+                }
+                BodyMedium(game.released.substringBefore("-"), color =  colorWhite )
+            }
+        }
+    }
+}
