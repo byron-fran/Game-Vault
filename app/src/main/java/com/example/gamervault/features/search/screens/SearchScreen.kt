eb969a337ee673bd0942014c32cb5a3bcd2a5a2e
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-11 15:31:58 -0800

    feat: Add initial SearchScreen
    Adds a new placeholder composable, `SearchScreen`, to serve as the foundation for the search feature.
    
    The screen currently consists of a `Column` with a `Text` element displaying "Search Screen" and includes status bar padding.

---
+++ app/src/main/java/com/example/gamervault/features/search/screens/SearchScreen.kt
@@ -0,0 +1,17 @@
+package com.example.gamervault.features.search.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchScreen() {
+
+    Column(
+        modifier = Modifier.statusBarsPadding()
+    ) {
+        Text("Search Screen")
+    }
+}
