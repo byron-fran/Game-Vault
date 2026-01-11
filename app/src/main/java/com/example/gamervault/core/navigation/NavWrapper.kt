200e0164c7d2daf3a663c1426cca16ab7d13fdf2
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-11 14:48:33 -0800

    feat: Add NavWrapper for navigation and screen composition
    Adds `NavWrapper`, a central composable responsible for managing the application's navigation graph using `navigation3`.
    
    It utilizes `SharedTransitionLayout` to enable shared element transitions between screens. The `NavDisplay` component is configured with an `entryProvider` that defines the composable destinations for the following routes:
    
    -   `GamesScreen`
    -   `GameDetailScreen`
    -   `FavoritesScreen`
    -   `SearchScreen`
    
    This component also handles navigation actions, such as navigating to the `GameDetailScreen` and handling back navigation. For the `GameDetailScreen`, it uses `hiltViewModel` to provide the `GamesViewModel`.

---
+++ app/src/main/java/com/example/gamervault/core/navigation/NavWrapper.kt
@@ -0,0 +1,51 @@
+package com.example.gamervault.core.navigation
+
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.ui.NavDisplay
+import com.example.gamervault.features.favorites.screens.FavoritesScreen
+import com.example.gamervault.features.games.screens.GameDetailScreen
+import com.example.gamervault.features.games.screens.GamesScreen
+import com.example.gamervault.features.games.viewmodel.GamesViewModel
+import com.example.gamervault.features.search.screens.SearchScreen
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+fun NavWrapper(backStack: NavBackStack<NavKey>) {
+
+
+    SharedTransitionLayout {
+        NavDisplay(
+            backStack = backStack,
+            onBack = { backStack.removeLastOrNull() },
+            entryProvider = entryProvider {
+
+                entry(key = Route.GameScreen) {
+                    GamesScreen { id ->
+                        backStack.add(Route.GameDetailScreen(id))
+                    }
+                }
+                entry<Route.GameDetailScreen> { args ->
+                    val gameViewModel: GamesViewModel = hiltViewModel()
+                    GameDetailScreen(
+                        id = args.id,
+                        onEvent = gameViewModel::onEvent,
+                        gamesUiState = gameViewModel.gameUiState.value,
+                        gameUiStatus = gameViewModel.gameUiStatus.value
+                    )
+                }
+                entry<Route.FavoritesScreen>{
+                    FavoritesScreen()
+                }
+                entry<Route.SearchScreen>{
+                    SearchScreen()
+                }
+            }
+        )
+    }
+}
