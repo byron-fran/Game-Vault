c408edc1999e7f8fbd0f8f1be412b914d122fa51
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-11 14:35:40 -0800

    feat: Implement custom color theme
    Replaces the default Material 3 color palette with a custom one tailored for the app's light and dark themes.
    
    - Defines a new set of color values in `Color.kt` for both light and dark modes, including background, primary, secondary, surface, and tertiary colors.
    - Updates `Theme.kt` to apply these new colors to the `LightColorScheme` and `DarkColorScheme`.
    - Disables dynamic color to ensure a consistent look across all Android versions.

--- app/src/main/java/com/example/gamervault/ui/theme/Color.kt
+++ app/src/main/java/com/example/gamervault/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.example.gamervault.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBackground = Color(0xFF101828) // Gray 900
+val DarkPrimary = Color(0xFF432dd7) // Indigo 700
+val DarkOnPrimary = Color(0xFFe0e7ff) // Indigo 200
+val DarkOnSecondary= Color(0xFF00b8db) // Cyan 500
+val DarkSurface = Color(0xFF1e2939) // Gray 800
+val DarkOnTertiary = Color(0xFFfcc800)
+val LightBackground = Color(0xFFe0e7ff) // Indigo 100
+val LightPrimary = Color(0xFF432dd7) // Indigo 700
+val LightOnPrimary = Color(0xFF101828) // Gray 900
+val LightOnSecondary= Color(0xFF0092b8) // Cyan 600
+val LightSurface = Color(0x40A3B3FF)
+val LightOnTertiary = Color(0xFFfcc800)
