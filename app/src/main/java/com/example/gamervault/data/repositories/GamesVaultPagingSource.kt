78ab99318be86a281c89e0409d198185768c126f
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-11 16:54:26 -0800

    feat: Implement PagingSource for fetching games
    Adds `GamesVaultPagingSource`, a `PagingSource` implementation for the Paging 3 library.
    
    This class is responsible for fetching paginated game data from the `GamesVaultApi`. It handles loading pages, managing page keys, and converting the remote data transfer objects (DTOs) into domain models using the `toDomain()` mapper. It also includes basic error handling for network requests.

---
+++ app/src/main/java/com/example/gamervault/data/repositories/GamesVaultPagingSource.kt
@@ -0,0 +1,41 @@
+package com.example.gamervault.data.repositories
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.gamervault.core.mappers.toDomain
+import com.example.gamervault.data.remote.GamesVaultApi
+import com.example.gamervault.domain.models.Game
+
+class GamesVaultPagingSource(
+    private val gamesVaultApi: GamesVaultApi
+): PagingSource<Int, Game>() {
+
+    override fun getRefreshKey(state: PagingState<Int, Game>): Int? {
+        return state.anchorPosition?.let { anchor ->
+            state.closestPageToPosition(anchor)?.prevKey?.plus(1)
+                ?:
+                state.closestPageToPosition(anchor)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Game> {
+
+        val pageIndex = params.key ?: 1
+        val pageSize = params.loadSize
+
+        return try {
+            val response = gamesVaultApi.getGames(pageIndex, pageSize)
+            LoadResult.Page(
+                data = response.body()?.games?.map { it.toDomain() } ?: emptyList(),
+                prevKey = if (pageIndex == 1) null else pageIndex - 1,
+                nextKey = if (response.body()?.games?.isEmpty() == true) null else pageIndex + 1
+            )
+
+        }catch (e: Exception){
+            LoadResult.Error(e)
+        }
+
+    }
+
+
+}
