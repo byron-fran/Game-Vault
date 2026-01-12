72345d3381daff012ec60e7e6dfe9b8da10ff004
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-11 16:46:17 -0800

    feat: Add initial FavoritesScreen
    Adds a new, empty Composable `FavoritesScreen` as a placeholder for the favorites feature.
    
    The screen includes basic setup with a `Column` and `statusBarsPadding` modifier. A `TODO` is left to indicate that more content needs to be added.

---
+++ app/src/main/java/com/example/gamervault/features/favorites/screens/FavoritesScreen.kt
@@ -0,0 +1,16 @@
+package com.example.gamervault.features.favorites.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FavoritesScreen() {
+    Column(
+        modifier = Modifier.statusBarsPadding()
+    ) {
+
+        // TODO add more content
+    }
+}
