006b231f65ba11475e67e86adfcf399aa0feee6f
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-11 16:52:29 -0800

    feat: Implement GamesVaultRepository
    Adds `GamesVaultRepositoryImpl`, the concrete implementation of the `GamesVaultRepository` interface.
    
    Key features include:
    - A `getGames` method that uses the Paging 3 library to create a `Flow<PagingData<Game>>`. It configures a `Pager` with a `GamesVaultPagingSource` to handle paginated data fetching from the remote API.
    - A `getGameById` suspend function that fetches a single game by its ID from the `GamesVaultApi` and maps the result to a domain model.

---
+++ app/src/main/java/com/example/gamervault/data/repositories/GamesVaultRepositoryImpl.kt
@@ -0,0 +1,43 @@
+package com.example.gamervault.data.repositories
+
+import android.net.ConnectivityManager.NetworkCallback
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.gamervault.core.mappers.toDomain
+import com.example.gamervault.data.remote.GamesVaultApi
+import com.example.gamervault.domain.models.Game
+import com.example.gamervault.domain.repositories.GamesVaultRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GamesVaultRepositoryImpl @Inject constructor(
+    private val gamesVaultApi: GamesVaultApi
+) : GamesVaultRepository, NetworkCallback() {
+
+    override fun getGames(
+        pageSize: Int,
+        enablePlaceHolders: Boolean,
+        prefetchDistance: Int,
+        initialLoadSize: Int,
+        maxCacheSize: Int
+    ): Flow<PagingData<Game>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = pageSize,
+                enablePlaceholders = enablePlaceHolders,
+                prefetchDistance = prefetchDistance,
+                initialLoadSize = initialLoadSize,
+                maxSize = maxCacheSize
+
+            ),
+            pagingSourceFactory = {
+                GamesVaultPagingSource(gamesVaultApi)
+            }
+        ).flow
+    }
+
+    override suspend fun getGameById(id: Int): Game? {
+        return gamesVaultApi.getGameById(id)?.toDomain()
+    }
+}
