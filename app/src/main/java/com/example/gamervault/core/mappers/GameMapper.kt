bb71fa98c1a63a0b1915cdffd1e7d84cc8c8f715
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-12 19:22:34 -0800

    feat: Add mappers for detailed game data
    Extends the mapping functionality to convert detailed game data transfer objects (DTOs) into domain models.
    
    New mappers include:
    - `GameDetailDto` to `Game`
    - `PlatformXDto` to `PlatformX`
    - `PlatformDto` to `Platform`
    - `RequirementsDto` to `Requirements`
    
    The existing `GameDto` to `Game` mapper is also updated to include `backgroundImage`, `rating`, and `released` fields.

--- app/src/main/java/com/example/gamervault/core/mappers/GameMapper.kt
+++ app/src/main/java/com/example/gamervault/core/mappers/GameMapper.kt
@@ -1,10 +1,17 @@
 package com.example.gamervault.core.mappers
 
+import com.example.gamervault.data.DTO.GameDetailDto
 import com.example.gamervault.data.DTO.GameDto
 import com.example.gamervault.data.DTO.GenreDto
+import com.example.gamervault.data.DTO.PlatformDto
+import com.example.gamervault.data.DTO.PlatformXDto
+import com.example.gamervault.data.DTO.RequirementsDto
 import com.example.gamervault.data.DTO.TagDto
 import com.example.gamervault.domain.models.Game
 import com.example.gamervault.domain.models.Genre
+import com.example.gamervault.domain.models.Platform
+import com.example.gamervault.domain.models.PlatformX
+import com.example.gamervault.domain.models.Requirements
 import com.example.gamervault.domain.models.Tag
 
 
@@ -12,12 +19,21 @@ fun GameDto.toDomain() = Game(
 
     id = this.id,
     name = this.name,
-    rating = this.rating,
     backgroundImage = this.backgroundImage,
+    rating = this.rating,
     released = this.released,
-    reviewsCount = this.reviewsCount,
-    reviewsTextCount = this.reviewsTextCount,
-    slug = this.slug
+)
+fun GameDetailDto.toDomain() = Game(
+    id = this.id,
+    description = this.description,
+    name = this.name,
+    backgroundImage = this.backgroundImage,
+    rating = this.rating,
+    released = this.released,
+    tags = this.tags.map { it.toDomain() },
+    genres = this.genres.map { it.toDomain() },
+    platforms = this.platforms.map { it.toDomain()},
+
 )
 
 fun GenreDto.toDomain() = Genre(
@@ -35,4 +51,21 @@ fun TagDto.toDomain() = Tag(
     name = this.name,
     slug = this.slug,
 
+)
+
+fun PlatformXDto.toDomain() = PlatformX(
+    platform = this.platform.toDomain(),
+    releasedAt = this.releasedAt,
+    requirements = this.requirements.toDomain()
+)
+
+fun PlatformDto.toDomain() = Platform(
+    id = this.id,
+    name = this.name,
+    slug = this.slug
+)
+
+fun RequirementsDto.toDomain() = Requirements(
+    minimum = this.minimum,
+    recommended = this.recommended
 )
