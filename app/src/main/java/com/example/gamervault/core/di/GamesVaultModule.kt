e123c03dd558f3bd7d91038067ed5054dcadee4f
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-14 11:22:00 -0800

    feat: Add SearchGamesUseCase to dependency injection graph
    Adds the `SearchGamesUseCase` to the Hilt dependency injection module.
    
    This makes the use case available to be injected throughout the application, allowing other components to perform game searches via the repository.

--- app/src/main/java/com/example/gamervault/core/di/GamesVaultModule.kt
+++ app/src/main/java/com/example/gamervault/core/di/GamesVaultModule.kt
@@ -5,6 +5,7 @@ import com.example.gamervault.data.repositories.GamesVaultRepositoryImpl
 import com.example.gamervault.domain.repositories.GamesVaultRepository
 import com.example.gamervault.domain.usecases.GetGameByIdUseCase
 import com.example.gamervault.domain.usecases.GetGamesUseCase
+import com.example.gamervault.domain.usecases.SearchGamesUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,6 +22,8 @@ object GamesVaultModule {
     @Provides
     fun provideGetGameByIdUseCase(repository: GamesVaultRepository) = GetGameByIdUseCase(repository)
 
+    @Provides
+    fun provideSearchGameUseCase(repository: GamesVaultRepository) = SearchGamesUseCase(repository)
     @Provides
     fun provideGamesVaultRepository(gamesVaultApi: GamesVaultApi) : GamesVaultRepository = GamesVaultRepositoryImpl(gamesVaultApi)
 
