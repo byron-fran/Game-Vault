191a56861fc5d4f700865d095a0f33f18f046713
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-14 12:03:26 -0800

    feat: Refine theme color palette
    Updates the color definitions for both the dark and light themes to improve visual consistency and accessibility.
    
    Changes include:
    - Modifying `DarkOnPrimary` for better contrast in the dark theme.
    - Adding `DarkOnSurface` and `LightOnSurface` to define text and icon colors on surface components.

--- app/src/main/java/com/example/gamervault/ui/theme/Color.kt
+++ app/src/main/java/com/example/gamervault/ui/theme/Color.kt
@@ -4,13 +4,16 @@ import androidx.compose.ui.graphics.Color
 
 val DarkBackground = Color(0xFF101828) // Gray 900
 val DarkPrimary = Color(0xFF432dd7) // Indigo 700
-val DarkOnPrimary = Color(0xFFe0e7ff) // Indigo 200
+val DarkOnPrimary = Color(0xFFcad5e2) // Gray 300
 val DarkOnSecondary= Color(0xFF00b8db) // Cyan 500
 val DarkSurface = Color(0xFF1e2939) // Gray 800
+val DarkOnSurface = Color(0xFF6a7282) // Gray 500
 val DarkOnTertiary = Color(0xFFfcc800)
+
 val LightBackground = Color(0xFFe0e7ff) // Indigo 100
 val LightPrimary = Color(0xFF432dd7) // Indigo 700
 val LightOnPrimary = Color(0xFF101828) // Gray 900
 val LightOnSecondary= Color(0xFF0092b8) // Cyan 600
 val LightSurface = Color(0x40A3B3FF)
+val LightOnSurface = Color(0xFF364153)
 val LightOnTertiary = Color(0xFFfcc800)
