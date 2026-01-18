961d617d13a8a1279eadac228c5e8ae7a22b1866
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-18 15:45:16 -0800

    feat: Add read-only states for form fields
    Extends the `FormUiState` data class to include read-only states for the email, username, and password fields.
    
    This allows for disabling user input on these fields when necessary.

--- app/src/main/java/com/example/gamervault/features/auth/states/FormUiState.kt
+++ app/src/main/java/com/example/gamervault/features/auth/states/FormUiState.kt
@@ -3,5 +3,8 @@ package com.example.gamervault.features.auth.states
 data class FormUiState(
     var hidePassword : Boolean = true,
     var errorEmail : String? = null,
-    var errorPassword : String? = null
+    var errorPassword : String? = null,
+    var readOnlyEmail : Boolean = false,
+    var readOnlyUsername : Boolean = false,
+    var readOnlyPassword : Boolean = false
 )
