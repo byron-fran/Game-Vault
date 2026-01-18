ed3528445fe0242ef2183ac9101b422941171665
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-18 07:01:53 -0800

    feat: Add FormViewModel for authentication
    Introduces `FormViewModel` to manage the state and logic for authentication forms.
    
    This ViewModel handles:
    - Storing and updating form data (email, password, username) using `DataForm` and `FormUiState`.
    - Toggling password visibility.
    - Validating email and password fields with specific error messages.
    - Enabling the submit button based on input.
    - Clearing errors and resetting the form state.

---
+++ app/src/main/java/com/example/gamervault/features/auth/viewmodel/FormViewModel.kt
@@ -0,0 +1,68 @@
+package com.example.gamervault.features.auth.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.gamervault.features.auth.states.DataForm
+import com.example.gamervault.features.auth.states.FormUiState
+import com.example.gamervault.features.auth.states.TypeField
+
+class FormViewModel : ViewModel() {
+    private var _formUiData = mutableStateOf(DataForm())
+    val formUiData = _formUiData
+    private var _formUiState = mutableStateOf(FormUiState())
+    val formUiState = _formUiState
+
+    fun onValueChange(value: String, field: TypeField) {
+        when (field) {
+            TypeField.EMAIL -> _formUiData.value = _formUiData.value.copy(email = value)
+            TypeField.PASSWORD -> _formUiData.value = _formUiData.value.copy(password = value)
+            TypeField.USERNAME -> _formUiData.value = _formUiData.value.copy(username = value)
+        }
+    }
+
+    fun enableButtonSubmit(): Boolean {
+        return formUiData.value.email.trim().isNotEmpty() && formUiData.value.password.trim().isNotEmpty()
+    }
+
+    fun hidePassword() {
+        _formUiState.value = _formUiState.value.copy(hidePassword = !_formUiState.value.hidePassword)
+    }
+
+    fun validateEmail(email: String, messageError : String): Boolean {
+        val emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}\$".toRegex()
+        val isEmailValid = emailRegex.matches(email)
+        if (!isEmailValid) {
+            _formUiState.value = _formUiState.value.copy(errorEmail = messageError)
+        } else {
+            _formUiState.value = _formUiState.value.copy(errorEmail = "")
+        }
+        return isEmailValid
+    }
+
+    fun validatePassword(password: String, messageError : String): Boolean {
+        if (password.length < 6) {
+            _formUiState.value = _formUiState.value.copy(
+                errorPassword = messageError
+            )
+            return false
+        } else {
+            _formUiState.value = _formUiState.value.copy(errorPassword = "")
+            return true
+        }
+    }
+
+    fun clearErrors() {
+        _formUiState.value = _formUiState.value.copy(
+            errorEmail = "",
+            errorPassword = ""
+        )
+    }
+
+    fun resetForm() {
+        _formUiData.value = _formUiData.value.copy(
+            email = "",
+            password = "",
+            username = ""
+        )
+    }
+}
