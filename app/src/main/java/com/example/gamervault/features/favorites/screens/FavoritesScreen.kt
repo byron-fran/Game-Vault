75804ca6585577536d3004c116448ab9376abce4
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-19 19:39:38 -0800

    feat(favorites): Build out FavoritesScreen UI and logic
    Implements the user interface for the Favorites screen, handling different states such as loading, error, and an empty list.
    
    Changes include:
    - Displaying a `LoadingScreen` while favorite games are being fetched.
    - Showing an `EmptyScreen` with a relevant message if there are no favorites or if an error occurs.
    - Rendering the list of favorite games using the `GamesListItems` composable.
    - Adding a more options icon button to each list item, which triggers a `CustomModalBottomSheet`.
    - The bottom sheet contains `FavoriteActions` to allow users to manage their favorite games.

--- app/src/main/java/com/example/gamervault/features/favorites/screens/FavoritesScreen.kt
+++ app/src/main/java/com/example/gamervault/features/favorites/screens/FavoritesScreen.kt
@@ -1,16 +1,83 @@
 package com.example.gamervault.features.favorites.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.gamervault.R
+import com.example.gamervault.core.mappers.toGame
+import com.example.gamervault.features.favorites.components.FavoriteActions
+import com.example.gamervault.features.favorites.viewmodel.FavoritesViewModel
+import com.example.gamervault.ui.components.BodyLarge
+import com.example.gamervault.ui.components.CustomIconButton
+import com.example.gamervault.ui.components.CustomModalBottomSheet
+import com.example.gamervault.ui.components.GamesListItems
+import com.example.gamervault.ui.screens.EmptyScreen
+import com.example.gamervault.ui.screens.LoadingScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FavoritesScreen() {
-    Column(
-        modifier = Modifier.statusBarsPadding()
-    ) {
+fun FavoritesScreen(
+    favoritesViewModel: FavoritesViewModel,
+    onNavigateToDetail: (Int) -> Unit
+) {
+    val favoritesUiState by favoritesViewModel.favoritesUiState
+    val favoriteSelected by favoritesViewModel.favoriteSelected
 
-        // TODO add more content
+    Column(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
+
+        when {
+            favoritesUiState.isLoading -> {
+                LoadingScreen(modifier = Modifier.fillMaxSize())
+            }
+
+            favoritesUiState.error != null -> {
+                EmptyScreen(modifier = Modifier.fillMaxSize()) {
+                    BodyLarge(text = favoritesUiState.error ?: "", maxLines = 3)
+                }
+            }
+
+            favoritesUiState.favorites.isEmpty() -> {
+                EmptyScreen(modifier = Modifier.fillMaxSize()) {
+                    BodyLarge(text = stringResource(R.string.no_favorites), maxLines = 3)
+                }
+            }
+
+            else -> {
+                val games = favoritesUiState.favorites.map { it.toGame() }
+                GamesListItems(
+                    games,
+                    action = { game ->
+                        CustomIconButton(icon = R.drawable.icon_more_vert) {
+                            favoritesViewModel.onFavoriteSelected(game)
+                        }
+                    }
+                ) {
+                    onNavigateToDetail(it)
+                }
+                favoriteSelected?.let { game ->
+                    CustomModalBottomSheet(
+                        modifier = Modifier,
+                        onDismissRequest = {
+                            favoritesViewModel.onFavoriteSelected(null)
+                        }
+                    ) {
+                        FavoriteActions(
+                            game = game,
+                            modifier = Modifier.height(400.dp)
+                        ) {
+                            favoritesViewModel.onClickFavorite(game)
+                            favoritesViewModel.onFavoriteSelected(null)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
