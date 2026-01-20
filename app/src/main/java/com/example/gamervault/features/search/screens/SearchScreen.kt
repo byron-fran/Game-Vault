8e9fda78d9addba6c1efbd2a7ea4a61471807074
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-19 19:41:37 -0800

    refactor: Replace ItemsSearchList with GamesListItems
    Replaces the `ItemsSearchList` composable with the more generic `GamesListItems` in `SearchScreen`. This change promotes code reuse by utilizing a shared component for displaying game lists.

--- app/src/main/java/com/example/gamervault/features/search/screens/SearchScreen.kt
+++ app/src/main/java/com/example/gamervault/features/search/screens/SearchScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.gamervault.R
 import com.example.gamervault.features.search.components.GameVaultSearchBar
-import com.example.gamervault.features.search.components.ItemsSearchList
+import com.example.gamervault.ui.components.GamesListItems
 import com.example.gamervault.features.search.events.SearchEvents
 import com.example.gamervault.features.search.states.SearchUiResponse
 import com.example.gamervault.features.search.states.SearchUiState
@@ -43,7 +43,7 @@ fun SearchScreen(
                 }
                 SearchUiResponse.Success -> {
                     if(searchUiState.games.isNotEmpty()) {
-                        ItemsSearchList(searchUiState.games) {
+                        GamesListItems(searchUiState.games) {
                             onNavigateToDetail(it)
                         }
                     }
