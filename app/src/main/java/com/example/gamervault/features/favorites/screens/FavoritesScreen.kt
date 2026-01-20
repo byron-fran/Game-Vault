e24544a5b2120560f552eb74b427bebc216f0ad3
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-20 12:52:09 -0800

    feat(favorites): Fetch favorites only when authenticated
    Fetches the user's favorite games only when they are authenticated. If the user is not signed in, a message is displayed prompting them to sign in or create an account.
    
    This is achieved by observing the authentication state and triggering the `getFavorites()` call within a `LaunchedEffect` once the user is authenticated.

--- app/src/main/java/com/example/gamervault/features/favorites/screens/FavoritesScreen.kt
+++ app/src/main/java/com/example/gamervault/features/favorites/screens/FavoritesScreen.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.gamervault.R
 import com.example.gamervault.core.mappers.toGame
+import com.example.gamervault.features.auth.viewmodel.AuthViewModel
 import com.example.gamervault.features.favorites.components.FavoriteActions
 import com.example.gamervault.features.favorites.viewmodel.FavoritesViewModel
 import com.example.gamervault.ui.components.BodyLarge
@@ -25,18 +27,30 @@ import com.example.gamervault.ui.screens.LoadingScreen
 @Composable
 fun FavoritesScreen(
     favoritesViewModel: FavoritesViewModel,
+    authViewModel: AuthViewModel,
     onNavigateToDetail: (Int) -> Unit
 ) {
     val favoritesUiState by favoritesViewModel.favoritesUiState
     val favoriteSelected by favoritesViewModel.favoriteSelected
+    val authUiState by authViewModel.authUiState
+
+    LaunchedEffect(authUiState.isAuthenticated) {
+        if(authUiState.isAuthenticated){
+            favoritesViewModel.getFavorites()
+        }
+    }
 
     Column(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
 
         when {
+            !authUiState.isAuthenticated -> {
+                EmptyScreen(modifier = Modifier.fillMaxSize()) {
+                    BodyLarge(text = stringResource(R.string.signin_or_create_account), maxLines = 3)
+                }
+            }
             favoritesUiState.isLoading -> {
                 LoadingScreen(modifier = Modifier.fillMaxSize())
             }
-
             favoritesUiState.error != null -> {
                 EmptyScreen(modifier = Modifier.fillMaxSize()) {
                     BodyLarge(text = favoritesUiState.error ?: "", maxLines = 3)
@@ -48,7 +62,6 @@ fun FavoritesScreen(
                     BodyLarge(text = stringResource(R.string.no_favorites), maxLines = 3)
                 }
             }
-
             else -> {
                 val games = favoritesUiState.favorites.map { it.toGame() }
                 GamesListItems(
