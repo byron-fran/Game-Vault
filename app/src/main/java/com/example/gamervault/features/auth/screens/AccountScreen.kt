076a17f860b7be900dd2f1045a79f3533c590a26
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-23 19:40:06 -0800

    refactor(auth): Decouple AccountScreen from ViewModels
    Decouples the `AccountScreen` composable from `AuthViewModel` and `FormViewModel` by introducing a stateless `AccountScreen` and a stateful `AccountRoute`.
    
    - `AccountRoute` is now responsible for providing the state from the ViewModels to the `AccountScreen`.
    - `AccountScreen` receives state (`AuthUiState`, `FormUiState`) and event callbacks (`signOut`, `onNavigateTo`) as parameters, making it more reusable and easier to test.
    - The `authViewModel` and `formViewModel` are now instantiated in `AccountRoute` using `hiltViewModel()` and `viewModel()` respectively.

--- app/src/main/java/com/example/gamervault/features/auth/screens/AccountScreen.kt
+++ app/src/main/java/com/example/gamervault/features/auth/screens/AccountScreen.kt
@@ -1,8 +1,5 @@
 package com.example.gamervault.features.auth.screens
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,18 +10,14 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.gamervault.R
 import com.example.gamervault.core.navigation.Route
-import com.example.gamervault.domain.models.User
+import com.example.gamervault.features.auth.states.AuthUiState
 import com.example.gamervault.features.auth.states.DataForm
 import com.example.gamervault.features.auth.states.FormUiState
 import com.example.gamervault.features.auth.viewmodel.AuthViewModel
@@ -32,41 +25,63 @@ import com.example.gamervault.features.auth.viewmodel.FormViewModel
 import com.example.gamervault.ui.components.BodyLarge
 import com.example.gamervault.ui.components.CustomButton
 import com.example.gamervault.ui.components.CustomIcon
-import com.example.gamervault.ui.components.CustomIconButton
 import com.example.gamervault.ui.components.CustomTextField
 import com.example.gamervault.ui.screens.EmptyScreen
 import com.example.gamervault.ui.screens.LoadingScreen
-import com.example.gamervault.ui.theme.GamerVaultTheme
 
 @Composable
-fun AccountScreen(
-    authViewModel: AuthViewModel,
-    formViewModel : FormViewModel = viewModel(),
+fun AccountRoute(
+    authViewModel: AuthViewModel = hiltViewModel(),
+    formViewModel: FormViewModel = viewModel(),
     onNavigateTo: (Route) -> Unit
 ) {
 
     val authUiState by authViewModel.authUiState
+    val formUiState by formViewModel.formUiState
 
-    Column(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
+    AccountScreen(
+        authUiState = authUiState,
+        formUiState = formUiState,
+        signOut = authViewModel::signOut,
+        onNavigateTo = onNavigateTo
+    )
+}
+
+@Composable
+fun AccountScreen(
+    authUiState: AuthUiState,
+    formUiState : FormUiState,
+    signOut: () -> Unit,
+    onNavigateTo: (Route) -> Unit
+) {
+
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .statusBarsPadding()) {
         when {
             authUiState.isLoading -> {
                 LoadingScreen(modifier = Modifier.fillMaxSize())
             }
+
             authUiState.isError?.isNotEmpty() == true -> {
                 EmptyScreen(modifier = Modifier.fillMaxSize()) {
                     BodyLarge(text = authUiState.isError!!, maxLines = 3)
                 }
             }
+
             else -> {
-                if (authUiState.isAuthenticated && authUiState.user != null ) {
+                if (authUiState.isAuthenticated && authUiState.user != null) {
                     authUiState.user?.let {
-                        AccountBody(dataForm = DataForm(
-                            email = it.email ?: "",
-                            username = it.username ?: "",
-                        ),
-                            formUiState = formViewModel.formUiState.value,
-                            modifier = Modifier.fillMaxSize()) {
-                            authViewModel.signOut()
+                        AccountBody(
+                            dataForm = DataForm(
+                                email = it.email ?: "",
+                                username = it.username ?: "",
+                            ),
+                            formUiState = formUiState,
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            signOut()
                         }
                     }
                 } else {
@@ -112,14 +127,14 @@ fun AccountBody(
 
 @Composable
 fun AccountDetail(
-    dataForm : DataForm,
+    dataForm: DataForm,
     formUiState: FormUiState
 ) {
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         CustomTextField(
-            value =dataForm.email,
+            value = dataForm.email,
             readOnly = formUiState.readOnlyEmail,
             onValueChange = {
                 //TODO update email
@@ -130,9 +145,9 @@ fun AccountDetail(
                 CustomIcon(icon = R.drawable.icon_outline_email)
             },
 
-        )
+            )
         CustomTextField(
-            value =dataForm.username,
+            value = dataForm.username,
             readOnly = formUiState.readOnlyUsername,
             onValueChange = {
                 // TODO update username
